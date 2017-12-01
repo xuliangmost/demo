629d69016b4ef06fc385aa824944c28364c55a16
xuliangmost <xuliang.most@gmail.com>
2017-12-02 00:44:53 +0800

    add select

---
+++ src/component/common/select_.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react'
+
+class Select_ extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      show: false,
+      selectValue: props.options[0].value
+    }
+  }
+
+  render () {
+    const height_ = 50;
+    let {options} = this.props;
+    return (
+      <div
+        onClick={() => {
+          this.setState({show: !this.state.show})
+        }}
+        style={{
+          width: 100,
+          height: this.state.show ? height_ * options.length + 50 : height_, overflow: 'hidden',
+          background: '#85E2FF',
+          transition: 'all 0.4s ease',
+        }}>
+        <p style={styles.p1}>{this.state.selectValue}</p>
+        {
+          options.map((ele, index) => {
+            return <p style={styles.p1} key={index}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        this.setState({
+                          selectValue: ele.value,
+                          show: false
+                        });
+                        this.props.onChange && this.props.onChange(ele.value)
+                      }}>{ele.value}</p>
+          })
+        }
+      </div>
+    )
+  }
+}
+
+const styles = {
+  p1: {
+    width: 100, height: 50,
+    lineHeight: '50px',
+    textAlign: 'center'
+  }
+};
+export default Select_
--- src/component/index/index.js
+++ src/component/index/index.js
@@ -3,79 +3,89 @@ import {connect} from 'react-redux'
 import {IndexAction} from "./actions";
 import './index.less'
 import {Link} from 'react-router-dom'
-import {Button,Icon} from 'antd'
-import axios from 'axios'
+import {Button} from 'antd'
+import Select from '../common/select_'
 
 class Index extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            list: [1, 2, 3, 4, 5, 6]
-        }
+  constructor (props) {
+    super(props);
+    this.state = {
+      list: [1, 2, 3, 4, 5, 6]
     }
+  }
 
-    clickValue(value) {
-        this.props.clickValue(value);
-        // axios.request({
-        //     url: '',
-        //     params: {},
-        //     data: {},
-        //     method: 'get||post',
-        //     headers: {},
-        // }).then(res => {
-        //
-        // }).catch(e => {
+  clickValue (value) {
+    this.props.clickValue(value);
+    // axios.request({
+    //     url: '',
+    //     params: {},
+    //     data: {},
+    //     method: 'get||post',
+    //     headers: {},
+    // }).then(res => {
+    //
+    // }).catch(e => {
 
-        // })
+    // })
 
-    }
+  }
 
-    componentDidMount() {
+  componentDidMount () {
 
-    }
+  }
 
-    render() {
-        return (
-            <div style={{
-                height: '100%',
-                backgroundColor: '#f2f2f2',
-                width: '100%'
-            }}>
-                <h1>我是index~~</h1>
-                <h2>点击的数字:{this.props.showValue ? this.props.showValue : '未点击'}</h2>
-                <ul style={{overflow: 'hidden'}}>
-                    {
-                        this.state.list.map((ele, index) => {
-                            return (
-                                <Button type='primary'
-                                    className='showVal'
-                                    style={this.props.showValue === ele ? {
-                                        background: '#92FFFF'
-                                    } : {background: '-webkit-linear-gradient(left, #ACFAFF, #F6EAFF)'}}
-                                    onClick={() => {
-                                        this.clickValue(ele)
-                                    }} key={index}>{ele}</Button>
-                            )
-                        })
-                    }
-                </ul>
-                <div className='forms'>
-                    <input id='inputs' type="checkbox"/>
-                    <label className='labels' htmlFor="inputs">
-                    </label>
-                </div>
-                <Link to='steps'>跳转到页面</Link>
-                <Button type='primary'> haha </Button>
-                <Icon type='right'></Icon>
-            </div>
-        )
-    }
+  render () {
+    return (
+      <div style={{
+        height: '100%',
+        backgroundColor: '#fff',
+        width: '100%'
+      }}>
+        <h1>我是index~~</h1>
+        <h2>点击的数字:{this.props.showValue ? this.props.showValue : '未点击'}</h2>
+        <ul style={{overflow: 'hidden'}}>
+          {
+            this.state.list.map((ele, index) => {
+              return (
+                <Button type='primary'
+                        className='showVal'
+                        style={this.props.showValue === ele ? {
+                          background: '#92FFFF'
+                        } : {background: '-webkit-linear-gradient(left, #ACFAFF, #F6EAFF)'}}
+                        onClick={() => {
+                          this.clickValue(ele);
+                        }} key={index}>{ele}</Button>
+              )
+            })
+          }
+        </ul>
+        <div className='forms'>
+          <input id='inputs' type="checkbox"/>
+          <label className='labels' htmlFor="inputs">
+          </label>
+        </div>
+
+        <Link to='steps'>跳转到页面</Link>
+
+        <Select
+          onChange={(value) => {
+            console.log(value)
+          }}
+          options={
+            [
+              {value: 'value1'},
+              {value: 'value2'},
+              {value: 'value13'}
+            ]}/>
+      </div>
+    )
+  }
 }
 
-function mapState(state) {
-    return {
-        showValue: state.showValue
-    }
+function mapState (state) {
+  return {
+    showValue: state.showValue
+  }
 }
 
 export default connect(mapState, {...IndexAction})(Index)
